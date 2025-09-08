ef82a94ca0c80b2c53b49fa6b85707e53c3fea21
pedro henrique <dnlsniperonly2@gmail.com>
2025-09-08 00:18:47 +0000

    beleza, pode aplicar

--- src/components/app/HeartsBackground.tsx
+++ src/components/app/HeartsBackground.tsx
@@ -25,7 +25,7 @@ export function HeartsBackground() {
   });
 
   return (
-    <div className="absolute top-0 left-0 w-full h-full -z-20 pointer-events-none overflow-hidden">
+    <div className="absolute top-0 left-0 w-full h-full pointer-events-none overflow-hidden">
       <div className="relative w-full h-full">
         {hearts.map(heart => (
           <div
@@ -34,7 +34,7 @@ export function HeartsBackground() {
             style={heart.style}
           >
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-full h-full">
-              <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/>
+              <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41 .81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/>
             </svg>
           </div>
         ))}
